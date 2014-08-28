ac1b1d1405f29b699dde48925afab325bdf6bfae
horimislime <horimislime@gmail.com>
2014-08-28 11:06:51 +0900

    add gapi call

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -3,10 +3,36 @@
 console.log(gapi);
 chrome.runtime.onInstalled.addListener(function (details) {
     console.log('previousVersion', details.previousVersion);
+
+    gapi.auth.authorize({'immediate': false}, function() {
+	console.log('auth success');
+    });
 });
 
 chrome.tabs.onUpdated.addListener(function (tabId) {
     chrome.tabs.get(tabId, function(tab) {
-      console.log('open url = ' + tab.url);
+	console.log('title = ' + tab.title + ' url = ' + tab.url);
+
+	var data = {
+	    "kind": "bigquery#tableDataInsertAllRequest",
+	    "rows": [
+		{
+		    "json": {
+			"title": tab.title,
+			"url": tab.url
+		    }
+		}
+	    ]
+	};
+	
+	gapi.client.request({
+	    'path': '/bigquery/v2/projects/pelagic-sorter-660/datasets/log/tables/weblog/insertAll',
+	    'method': 'POST',
+	    'body': data,
+	    'callback': function(json,raw) {
+		console.log('json resp = '+JSON.stringify(json));
+		console.log('raw resp = '+raw);
+	    }
+	});
     });
 });
