6fe19bb4448b6313c0977524daf0bf950615be04
horimislime <horimislime@gmail.com>
2014-09-11 22:10:26 +0900

    add storage support

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,9 +1,24 @@
 $(document).ready(function() {
+    
+    chrome.storage.sync.get({
+	'projectId': '',
+	'datasetId': '',
+	'tableId': ''
+    }, function(items) {
+	console.log(items);
+	$('#project-id').val(items.projectId);
+	$('#dataset-id').val(items.datasetId);
+	$('#table-id').val(items.tableId);
+    });
+    
     $('#save').click( function() {
-	console.log('click');
-	localStorate['project-id'] = $('#project-id').val();
-	localStorage['dataset-id'] = $('#dataset-id').val();
-	localStorage['table-id'] = $('#table-id').val();
-	console.log(JSON.stringify(localStorage));
+	console.log('val='+$('#project-id').val());
+	chrome.storage.sync.set({
+	    'projectId': $('#project-id').val(),
+	    'datasetId': $('#dataset-id').val(),
+	    'tableId': $('#table-id').val()
+	}, function() {
+	    alert('Settings saved.');
+	});
     });
 });
